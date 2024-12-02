3f4382559978908937f5efdd38a04ebb3cd5eda1
krishna <srik51977@gmail.com>
2024-12-02 12:53:28 +0530

    Added Features for CK Editor

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,64 +1,67 @@
 @extends('admin.layout.app')
 
 @section('content')
-<section class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1>Dashboard</h1>
-            </div>
-            <div class="col-sm-6">
-
-            </div>
-        </div>
-    </div>
-    <!-- /.container-fluid -->
-</section>
-<!-- Main content -->
-<section class="content">
-    <!-- Default box -->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-4 col-6">
-                <div class="small-box card">
-                    <div class="inner">
-                        <h3>150</h3>
-                        <p>Total Cars</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
-                    </div>
-                    <a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Dashboard</h1>
                 </div>
-            </div>
+                <div class="col-sm-6">
 
-            <div class="col-lg-4 col-6">
-                <div class="small-box card">
-                    <div class="inner">
-                        <h3>50</h3>
-                        <p>Booking Cars</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-stats-bars"></i>
-                    </div>
-                    <a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-6">
-                <div class="small-box card">
-                    <div class="inner">
-                        <h3>100</h3>
-                        <p>Available Cars</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-person-add"></i>
-                    </div>
-                    <a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- /.card -->
-</section>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <!-- Default box -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4 col-6">
+                    <div class="small-box card">
+                        <div class="inner">
+                            <h3>150</h3>
+                            <p>Total Cars</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="#" class="small-box-footer text-dark">More info <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-6">
+                    <div class="small-box card">
+                        <div class="inner">
+                            <h3>50</h3>
+                            <p>Booking Cars</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-stats-bars"></i>
+                        </div>
+                        <a href="#" class="small-box-footer text-dark">More info <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-6">
+                    <div class="small-box card">
+                        <div class="inner">
+                            <h3>100</h3>
+                            <p>Available Cars</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person-add"></i>
+                        </div>
+                        <a href="#" class="small-box-footer text-dark">More info <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.card -->
+    </section>
 @endsection
--- resources/views/admin/setting/view.blade.php
+++ resources/views/admin/setting/view.blade.php
@@ -6,10 +6,18 @@
 
         <form action="{{ route("$section.store", ['section' => $section]) }}" method="POST">
             @csrf
-            <div class="form-group">
-                <textarea required name="content" id="content" class="form-control wysiwyg">
+            <div class="form-group editor-container">
+                <!-- Toolbar container -->
+                <div id="editor-toolbar"></div>
+                <!-- Menu bar container (optional) -->
+                <div id="editor-menu-bar"></div>
+                <!-- The editable div (CKEditor content) -->
+                <div required class="form-control wysiwyg" style="height: 50vh">
                     {!! !empty($item['data_values']) ? json_decode($item['data_values']) : '' !!}
-                </textarea>
+                </div>
+                <!-- Hidden input to store the content -->
+                <input type="hidden" id="content-input" name="content"
+                    value="{{ old('content', !empty($item['data_values']) ? json_decode($item['data_values']) : '') }}">
                 <input type="hidden" name="{{ $section }}_id" value="{{ $item['id'] ?? 0 }}">
             </div>
             <button type="submit" class="btn btn-primary">Save</button>
@@ -18,10 +26,83 @@
 @endsection
 
 @section('customJs')
-    <script src="https://cdn.ckeditor.com/ckeditor5/39.0.1/classic/ckeditor.js"></script>
     <script>
-        document.querySelectorAll('.wysiwyg').forEach((editor) => {
-            ClassicEditor.create(editor).catch(error => console.error(error));
+        const editorConfig = {
+            toolbar: {
+                items: [
+                    'undo', 'redo', '|', 'heading', '|', 'fontSize', 'fontFamily',
+                    'fontColor', 'fontBackgroundColor', '|', 'bold', 'italic', 'underline',
+                    '|', 'link', 'insertTable', '|', 'alignment', '|', 'outdent', 'indent'
+                ],
+                shouldNotGroupWhenFull: false
+            },
+            fontFamily: {
+                supportAllValues: true
+            },
+            fontSize: {
+                options: [10, 12, 14, 'default', 18, 20, 22],
+                supportAllValues: true
+            },
+            heading: {
+                options: [{
+                        model: 'paragraph',
+                        title: 'Paragraph',
+                        class: 'ck-heading_paragraph'
+                    },
+                    {
+                        model: 'heading1',
+                        view: 'h1',
+                        title: 'Heading 1',
+                        class: 'ck-heading_heading1'
+                    },
+                    {
+                        model: 'heading2',
+                        view: 'h2',
+                        title: 'Heading 2',
+                        class: 'ck-heading_heading2'
+                    },
+                    {
+                        model: 'heading3',
+                        view: 'h3',
+                        title: 'Heading 3',
+                        class: 'ck-heading_heading3'
+                    },
+                ]
+            },
+            link: {
+                addTargetToExternalLinks: true,
+                defaultProtocol: 'https://',
+                decorators: {
+                    toggleDownloadable: {
+                        mode: 'manual',
+                        label: 'Downloadable',
+                        attributes: {
+                            download: 'file'
+                        }
+                    }
+                }
+            },
+            table: {
+                contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+            },
+            placeholder: 'Type or paste your content here!'
+        };
+
+        document.querySelectorAll('.wysiwyg').forEach((element) => {
+            DecoupledEditor.create(element, editorConfig)
+                .then(editor => {
+                    // Append the toolbar to the editor-toolbar div
+                    document.querySelector('#editor-toolbar').appendChild(editor.ui.view.toolbar.element);
+
+                    // Set up the event listener to track changes and update the hidden input
+                    editor.model.document.on('change:data', () => {
+                        const content = editor.getData();
+                        document.querySelector('#content-input').value = content;
+                    });
+
+                    return editor;
+                })
+                .catch(error => console.error(error));
         });
     </script>
 @endsection
