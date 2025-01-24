0954c1f9bbc34d526fcece68f3bbc5e6a888dc2b
saranraj0018 <saranrajhuk0018@gmail.com>
2025-01-24 18:12:33 +0530

    valam bugs

--- app/Message/BookingRescheduledMessage.php
+++ app/Message/BookingRescheduledMessage.php
@@ -35,11 +35,11 @@ class BookingRescheduledMessage implements Message {
 
         $_name = ucfirst($this->booking->user->name);
         $_id = $this->booking->booking_id;
-        $_drop = $this->booking->location()->dropoff;
         $_from = $this->booking->start_date;
+        $_to = $this->booking->end_date;
         $_car = $this->booking?->Car?->carModel?->model_name;
 
-        return "Dear $_name, Your booking with Valam Cars has been successfully modified. Updated Booking Details: Booking ID: $_id, New Drop-off Date/Time: $_drop, New Pick-up Date/Time: $_from, Car Model: $_car, If you have any questions or need further assistance, please visit our website or contact customer support. We look forward to serving you! Valam Cars Team";
+        return "Dear $_name, Your booking with Valam Cars has been successfully modified. Updated Booking Details: Booking ID: $_id, New Drop-off Date/Time: $_from, New Pick-up Date/Time: $_to, Car Model: $_car, If you have any questions or need further assistance, please visit our website or contact customer support. We look forward to serving you! Valam Cars Team";
     }
 }
 
