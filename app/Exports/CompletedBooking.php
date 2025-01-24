c27a4407905ff7605fe45645229e6c875901d035
krishna <srik51977@gmail.com>
2025-01-24 15:27:54 +0530

    Excel Export for Completed Booking

--- app/Exports/CompletedBooking.php
+++ app/Exports/CompletedBooking.php
@@ -22,6 +22,8 @@ class CompletedBooking implements FromCollection, WithHeadings {
     public function headings(): array {
         return [
             'Booking Type',
+            'Booking ID',
+            'Status',
             'Start Date',
             'End Date',
             'User Name',
@@ -30,6 +32,9 @@ class CompletedBooking implements FromCollection, WithHeadings {
             'Register Number',
             'Address',
             'Reschedule Date',
+            'Latitude',
+            'Longitude',
+            'Created Date'
         ];
     }
 
@@ -41,20 +46,24 @@ class CompletedBooking implements FromCollection, WithHeadings {
 
         $booking = Booking::with(['user', 'details', 'comments', 'user.bookings','details', 'Car' ,'Car.carModel'])
             ->where('city_code', $this->hub)
-            ->where('status', 2)
             ->get();
 
         return $booking->map(function($book) {
             return collect([
-                $book?->book_type,
-                $book?->start_date,
-                $book?->end_date,
-                $book?->user->name,
-                $book?->user->mobile,
+                $book?->booking_type,
+                $book?->booking_id,
+                ($book?->status == '1' ? 'Booking': ($book?->status == '2' ? 'Completed' : 'Canceled')),
+                \Carbon\Carbon::parse($book?->start_date)->format('d-m-Y H:i A'),
+                \Carbon\Carbon::parse($book?->end_date)->format('d-m-Y H:i A'),
+                $book?->user?->name,
+                $book?->user?->mobile,
                 $book?->Car?->carModel?->model_name,
                 $book?->Car?->register_number,
                 $book?->address,
-                $book?->reschedule_date,
+                \Carbon\Carbon::parse($book?->reschedule_date)->format('d-m-Y H:i A'),
+                $book?->latitude,
+                $book?->longitude,
+                \Carbon\Carbon::parse($book?->created_at)->format('d-m-Y H:i A'),
             ]);
         });
     }
